87a6a18a0ff70dc0e7194bed28ec14fd396fe255
violiveiradev <violiveira.dev@gmail.com>
2025-01-26 22:03:20 -0300

    Create models

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from stocks.models import Stock
 
-# Register your models here.
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'moment',)
+    search_fields = ('name',)
+    list_filter = ('name',) 
